93ead65a4ed43417e625ff2d1049511950a383b2
zoubaidas <36661627+zoubaidas@users.noreply.github.com>
2020-10-15 22:29:16 +0200

    Upgrade local project to gitHub

--- databases/includes.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/interfaces/GlobalPDO.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/interfaces/MessageInterface.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/interfaces/UserInterface.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/interfaces/Utils.php';
-session_start();//démarrage de la session
-?>
