a9e6ce2a288aaee9ccbd877731ff36db0dce5667
Maxime Dupont <maxime.dupont@heig-vd.ch>
2020-10-16 15:09:10 +0200

    tweaked the scripts and added one for windows

--- stop.sh
+++ stop.sh
@@ -1,4 +1,5 @@
 #stop the conatainer
 docker stop Twink
+docker rm Twink
 #remove the image
 docker rmi -f arubinst/sti:project2018
--- winStart.sh
+++ winStart.sh
@@ -1,5 +1,8 @@
+docker kill Twink
+docker rm Twink
+
 # pull and run a container named "Twink"
-docker run -ti -v "$PWD/site":/usr/share/nginx/ -d -p 8080:80 --name Twink --hostname sti arubinst/sti:project2018
+docker run -ti -v "/$PWD/":/usr/share/nginx/ -d -p 8080:80 --name Twink --hostname sti arubinst/sti:project2018
 
 # run the web and PHP servers 
 docker exec -u root Twink service nginx start
