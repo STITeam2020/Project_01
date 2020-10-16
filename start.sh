5b175bc46fc797e96256a542ecb6b4304e27c6f6
Massaoudi Walid <39918257+WalidMassaoudi@users.noreply.github.com>
2020-10-16 18:28:27 +0200

    clean script

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ docker rm Twink
 
 # pull and run a container named "Twink"
 docker run -ti -v "/$PWD/site":/usr/share/nginx/ -d -p 8080:80 --name Twink --hostname sti arubinst/sti:project2018
-
 # run the web and PHP servers 
 docker exec -u root Twink service nginx start
 docker exec -u root Twink service php5-fpm start
