64211a9485cdaad83927f554a0cab177e17f1549
Massaoudi Walid <39918257+WalidMassaoudi@users.noreply.github.com>
2020-10-16 18:30:22 +0200

    clean feature delete

--- site/html/deleteUser.php
+++ site/html/deleteUser.php
@@ -3,8 +3,8 @@
 use dataManager\Db;
 use controller\User;
 include_once $_SERVER['DOCUMENT_ROOT'] . '/includes.php';
-
 $user = User::lookForUser($_GET["email"]);
 Db::deleteUser($user->email);
 header("Location: confirmation.php");
+
 exit;
