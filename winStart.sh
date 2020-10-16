2e231d05ed79a654c8e8ef64937b045e77af50e6
Maxime Dupont <maxime.dupont@heig-vd.ch>
2020-10-16 15:15:49 +0200

    final touches to scripts

--- winStart.sh
+++
@@ -1,9 +0,0 @@
-docker kill Twink
-docker rm Twink
-
-# pull and run a container named "Twink"
-docker run -ti -v "/$PWD/":/usr/share/nginx/ -d -p 8080:80 --name Twink --hostname sti arubinst/sti:project2018
-
-# run the web and PHP servers 
-docker exec -u root Twink service nginx start
-docker exec -u root Twink service php5-fpm start
